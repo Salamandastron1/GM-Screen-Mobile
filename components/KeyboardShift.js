80d776e4297f0647b0d1ea947d59ea57b32cd8ff
Tim Garrity <tag.garrity@gmail.com>
2018-12-21 20:06:15 -0700

    start fixing keyboard and input

---
+++ components/KeyboardShift.js
@@ -0,0 +1,90 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Animated, Dimensions, Keyboard, TextInput, UIManager } from 'react-native';
+
+const { State: TextInputState } = TextInput;
+
+export default class KeyboardShift extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      shift: new Animated.Value(0),
+    };
+  }
+
+  componentDidMount() {
+    this.keyboardDidShowSub = Keyboard.addListener('keyboardDidShow', this.handleKeyboardDidShow);
+    this.keyboardDidHideSub = Keyboard.addListener('keyboardDidHide', this.handleKeyboardDidHide);
+  }
+
+  componentWillUnmount() {
+    this.keyboardDidShowSub.remove();
+    this.keyboardDidHideSub.remove();
+  }
+
+  handleKeyboardDidShow(e) {
+    const { height: windowHeight } = Dimensions.get('window');
+    const keyboardHeight = e.endCoordinates.height;
+    const currentlyFocusedField = TextInputState.currentlyFocusedField();
+
+    UIManager.measure(currentlyFocusedField, (originX, originY, width, height, pageX, pageY) => {
+      const fieldHeight = height;
+      const fieldTop = pageY;
+      const gap = (windowHeight - keyboardHeight) - (fieldTop + fieldHeight);
+      const { shift } = this.state;
+
+      if (gap >= 0) {
+        return;
+      }
+      Animated.timing(
+        shift,
+        {
+          toValue: gap,
+          duration: 1000,
+          useNativeDriver: true,
+        },
+      ).start();
+    });
+  }
+
+  handleKeyboardDidHide() {
+    const { shift } = this.state;
+    Animated.timing(
+      shift,
+      {
+        toValue: 0,
+        duration: 1000,
+        useNative: true,
+      },
+    ).start();
+  }
+
+  render() {
+    const { children } = this.props;
+    const { shift } = this.state;
+
+    return (
+      <Animated.View style={[styles.container, { transform: [{ translateY: shift }] }]}>
+        {children}
+      </Animated.View>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    height: '100%',
+    left: 0,
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+  },
+};
+
+KeyboardShift.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.array,
+  ]).isRequired,
+};
