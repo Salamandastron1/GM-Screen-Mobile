45e5a3f9604f372efb9b31ed4f8986950e11eacb
Tim Garrity <tag.garrity@gmail.com>
2018-12-30 20:37:02 -0700

    create playerscreen route, pass props via passing parameters in routes

--- components/PlayerScreen.js
+++ components/PlayerScreen.js
@@ -1,8 +1,41 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import MainContain from './MainContain';
+import CardSection from './CardSection';
+import * as API from '../helpers/API';
+import Title from './Title';
 
-const PlayerScreen = () => (
-  <View />
-);
+class PlayerScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerName: 'Tuchanka',
+      items: [],
+    };
+  }
+
+  async componentDidMount() {
+    const url = '/api/v1/characters/:id';
+    const items = await API.apiCall(url);
+
+    this.setState({
+      items,
+    });
+  }
+
+  render() {
+    const { playerName, items } = this.state;
+    return (
+      <MainContain>
+        <Title>
+          {playerName}
+        </Title>
+        <CardSection>
+          {items}
+        </CardSection>
+      </MainContain>
+    );
+  }
+}
 
 export default PlayerScreen;
